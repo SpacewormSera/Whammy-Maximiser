1a95b334a71c61670cb2aa465471f2ce823fc03d
spacewormsera <deluxe87d@gmail.com>
2023-11-15 11:27:53 +0100

    cofig file loading added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const maxApi = require('max-api')
+const fs = require('fs')
+
 
 maxApi.post('---> Script started')
+loadConfiguration()
 
 let variation = 0
-
+const configPath = './config.json';
 const variations = [
   [1,3,4,6,7],
   [2,3,5],
@@ -13,6 +16,13 @@ const variations = [
   [1,3,5,7],
   [1, 2, 3, 4, 5, 6, 7],
 ]
+
+function loadConfiguration(){
+  if(fs.existsSync(configPath)){
+    variations = fs.readFileSync(configPath, { encoding: 'utf8', flag: 'r' })
+    // todo send a signal to a led of successful loading
+  }
+}
 
 function getVariation(variationNumber) {
   const variationArr = variations[variationNumber]
@@ -32,18 +42,19 @@ function mapControlChangeToWhammy(midiValue, steps, mapArray) {
   return mapArray[pointer]
 }
 
-
 ///////////////////=-HANDLERS-=/////////////////////
 
+// main logic
 maxApi.addHandler('midi', (ccNum, ccVal) => {
   ccVal = 127 - ccVal
   // maxApi.post('---> midiValue pre ', ccNum, ccVal)
 
-  if (ccNum !== 123) {
+  if (ccNum !== 123) { // prevenr Ableton's sending CC-123 when a track stops
     maxApi.outlet(mapControlChangeToWhammy(ccVal, variations[variation].length, getVariation(variation)))
   }
 })
 
+// handle the 'variation' knob
 maxApi.addHandler('variation', (variationVal) => {
   // maxApi.post('---> varValue in ', variationVal)
   variation = variationVal
